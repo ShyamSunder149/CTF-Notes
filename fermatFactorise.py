1e8591f6c1774ec463f694cde4f2072831c14782
Shyam Sunder Saravanan <SubstantialCitron0@protonmail.com>
2021-06-28 15:08:45 +0000

    Create fermatFactorise.py

---
+++ fermatFactorise.py
@@ -0,0 +1,18 @@
+# Fermat Factorization : showing an odd number as a difference of squares of 2 numbers
+
+n = a**2 - b**2
+
+# this can be written as
+
+n = (a+b)*(a-b)
+
+# comparing with p and q
+
+p = a+b
+q = a-b
+# as n = pq
+
+#therefore resolving a and b
+
+a = 0.5*(p+q)
+b = 0.5*(q-p)
