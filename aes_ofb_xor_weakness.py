f75c3eeee1894aabe109a4fc2b2b882b5965daea
Shyam Sunder Saravanan <SubstantialCitron0@protonmail.com>
2022-01-12 12:32:18 +0530

    Update aes_ofb_xor_weakness.py

--- aes_ofb_xor_weakness.py
+++ aes_ofb_xor_weakness.py
@@ -1,17 +1,8 @@
+from pwn import *
 from binascii import unhexlify
 from Crypto.Cipher import AES
 
-re = remote('gc1.eng.run',30193)
-
-def encrypt(msg, iv):
-    cipher = AES.new(key, AES.MODE_OFB,iv)
-    ct = cipher.encrypt(msg)
-    return ct if ct not in flag else b"try_harder"
-
-def decrypt(msg, iv, key):
-    cipher = AES.new(key, AES.MODE_OFB,iv)
-    ct = cipher.decrypt(msg)
-    return ct
+re = remote('gc1.eng.run',30529)
 
 
 context.log_level = 'DEBUG'
@@ -20,16 +11,18 @@ re.recvuntil(b'\n')
 c = re.recvline()
 c = c.strip(b'\n')
 c = unhexlify(c.strip(b'\n'))
-
 re.recv(1024)
-p1 = (c[:16]+b'b'*16)
+p1 = (c[:16]+b'b'*64)
 re.sendline(str(p1.hex()))
 c1 = re.recvline()
 c1 = unhexlify(c1.strip(b'\n'))
 re.recv(1024)
-key1 = xor(c1,p1[16:])
+key1 = xor(c1[:16],p1[16:32])
 part1 = xor(key1,c[16:32])
-part2 = decrypt(c[16:32],c[:16],key1)
-print(part1)
-print(part2)
+key2 = xor(c1[16:32],p1[32:48])
+part2 = xor(key2,c[32:48])
+key3 = xor(c1[32:48],p1[48:52])
+part3 = xor(key3,c[48:52])
+print(part1+part2+part3)
+print(len(c1))
 print(len(c))
